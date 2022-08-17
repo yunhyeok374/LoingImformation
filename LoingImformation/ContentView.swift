c45aaadaf41b2790aa59c1cd254b4823f217aa4f
YunHyeok <djbyh645047@naver.com>
2022-08-17 13:22:51 +0900

    Initial Commit

---
+++ LoingImformation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoingImformation
+//
+//  Created by 배윤혁 on 2022/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoingImformation/LoingImformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoingImformationApp.swift
+//  LoingImformation
+//
+//  Created by 배윤혁 on 2022/08/17.
+//
+
+import SwiftUI
+
+@main
+struct LoingImformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
