969893cf0876f51725815f34a534a3821ac8a120
YunHyeok <djbyh645047@naver.com>
2022-08-18 23:41:02 +0900

    loginView

--- LoingImformation/View/LoginView.swift
+++ LoingImformation/View/LoginView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var id = ""
+    @State private var password = ""
     var body: some View {
         VStack {
-            Text("Name")
+            Text("Welcome!")
                 .font(.largeTitle)
-                .offset(y: -150)
+                .offset(y: -100)
                 .foregroundColor(.green)
+            TextField("ID", text: $id)
+                .frame(height: 55)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .cornerRadius(16)
+                .padding([.leading, .trailing], 24)
+            SecureField("Password", text: self.$password)
+                .frame(height: 55)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .cornerRadius(16)
+                .padding([.leading, .trailing], 24)
+            Button(action: {
+            
+            }, label:  {
+                Text("로그인")
+                    .foregroundColor(.green)
+                    .frame(width: 200)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 30)
+                            .fill(Color.white)
+                            .shadow(color: .gray, radius: 2, x: 0, y: 2)
+            )
+                    .padding()
+            })
+            
                 
         }
     }
